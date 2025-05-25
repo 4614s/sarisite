6224bc02e0bf283943ad4613074897dd51fa194a
4614s <onurgral1446.2@gmail.com>
2025-05-25 14:29:54 +0300

    Update scriptx.js

--- asetler/scriptx.js
+++ asetler/scriptx.js
@@ -4,7 +4,7 @@ function gonderiolustur() {
     "Gönderi Paylaşmak İçin Sosyal Medya Hesaplarımızdan Birinden Her hangi bir gönderimizin altına Gönderi Oluşturacağınızı Belirtip Gönderinizin Başlığını ve içeriğini yorum olarak atın. En Kısa Sürede Paylaşımınız SarıSiteye Eklenecektir."
   );
 }
-funtion bizeUlasin() {
+function bizeUlasin() {
 alert("Bize şuan ulaşamazsınız çünkü biz de bilmiyoruz nerde olduğumuzu:)");
 }
 
