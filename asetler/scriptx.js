367c9cde1a49cfa71a72133f67f465d165531145
4614s <onurgral1446.2@gmail.com>
2025-05-25 14:17:21 +0300

    Update scriptx.js

--- asetler/scriptx.js
+++ asetler/scriptx.js
@@ -4,6 +4,10 @@ function gonderiolustur() {
     "Gönderi Paylaşmak İçin Sosyal Medya Hesaplarımızdan Birinden Her hangi bir gönderimizin altına Gönderi Oluşturacağınızı Belirtip Gönderinizin Başlığını ve içeriğini yorum olarak atın. En Kısa Sürede Paylaşımınız SarıSiteye Eklenecektir."
   );
 }
+funtion bizeUlasin() {
+alert("Bize şuan ulaşamazsınız çünkü biz de bilmiyoruz nerde olduğumuzu:)");
+}
+
 let tarih = new Date();
 let guncelYil = tarih.getFullYear();
 document.getElementById("year").textContent = guncelYil;
