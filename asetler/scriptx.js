8dc623066e1cd11f0f8db23b0521067c6ca4e22b
4614s <onurgral1446.2@gmail.com>
2025-06-10 22:04:36 +0300

    Update scriptx.js

--- asetler/scriptx.js
+++ asetler/scriptx.js
@@ -1,4 +1,20 @@
 //Sarisite giriş yapılmış sayfa JS dosyası
+function isimAyarla() {
+  let isim = prompt("İsminiz (Sadece siz görürsünüz) :");
+ if(isim) {
+   localStorage.setItem("isim",isim);
+mesajgoster(isim);
+ }
+}
+function mesajgoster(isim) {
+  document.getElementById("hg-mesaji").innerText = `Hoş geldin, ${isim}!`;
+  document.getElementById("isimayarlabutonu").style.display = "none";
+}
+
+let kayitliIsim = localStorage.getItem("isim");
+    if (kayitliIsim) {
+      mesajGoster(kayitliIsim);
+    }
 function gonderiolustur() {
   alert(
     "Gönderi Paylaşmak İçin Sosyal Medya Hesaplarımızdan Birinden Her hangi bir gönderimizin altına Gönderi Oluşturacağınızı Belirtip Gönderinizin Başlığını ve içeriğini yorum olarak atın. En Kısa Sürede Paylaşımınız SarıSiteye Eklenecektir."
