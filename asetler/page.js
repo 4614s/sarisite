7500b06aa5dfae0bd4c073426a97d172919da2bd
4614s <onurgral1446.2@gmail.com>
2025-06-27 04:34:51 +0300

    Update page.js

--- asetler/page.js
+++ asetler/page.js
@@ -192,6 +192,47 @@ if(localStorage.getItem("giris") === "true"){
     <br/>
       
     </footer>`;
+ 
+document.querySelector("#isimayarlabutonu").addEventListener("click", isimAyarla);
+
+document.querySelector("#gonderiolustur").addEventListener("click", gonderiolustur);
+
+function isimAyarla() {
+  let isim = prompt("İsminiz (Sadece siz görürsünüz):");
+  if (isim) {
+    localStorage.setItem("isim", isim);
+    mesajGoster(isim);
+  }
+}
+
+function mesajGoster(isim) {
+  document.getElementById("hg-mesaji").innerText = `Hoş geldin, ${isim}!`;
+  document.getElementById("isimayarlabutonu").style.display = "none";
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  let kayitliIsim = localStorage.getItem("isim");
+  if (kayitliIsim) {
+    mesajGoster(kayitliIsim);
+  }
+ 
+  
+  let tarih = new Date();
+  let guncelYil = tarih.getFullYear();
+  document.getElementById("year").textContent = guncelYil;
+});
+ 
+
+function gonderiolustur() {
+  alert(
+    "Gönderi Paylaşmak İçin Sosyal Medya Hesaplarımızdan birinden herhangi bir gönderimizin altına gönderi oluşturacağınızı belirtip başlık + içeriği yorum olarak yazın. En kısa sürede paylaşılır."
+  );
+}
+
+function bizeUlasin() {
+  alert("Bize şu an ulaşamazsınız çünkü biz de bilmiyoruz neredeyiz :)");
+}
 }
  
 
@@ -211,7 +252,7 @@ else{
     <br />
     <br />
     <div style="display:flex; justify-content:center; align-item: center; padding: 9px;">
-    <a href="../sarisite/giris/" style="color: black:font-size: 1.2rem;">
+    <a href="../sarisite/giris/" style="color: black;font-size: 1.2rem;">
       Giriş Yap! ve Tüm Özelliklere Eriş</a
     >
     </div>
@@ -228,48 +269,7 @@ else{
 }
 
 
+
  
- 
-function isimAyarla() {
-  let isim = prompt("İsminiz (Sadece siz görürsünüz):");
-  if (isim) {
-    localStorage.setItem("isim", isim);
-    mesajGoster(isim);
-  }
-}
-
-function mesajGoster(isim) {
-  document.getElementById("hg-mesaji").innerText = `Hoş geldin, ${isim}!`;
-  document.getElementById("isimayarlabutonu").style.display = "none";
-}
-
-// DOM tamamen yüklenince kontrol et
-document.addEventListener("DOMContentLoaded", () => {
-  let kayitliIsim = localStorage.getItem("isim");
-  if (kayitliIsim) {
-    mesajGoster(kayitliIsim);
-  }
- 
-  // Yıl güncellemesi
-  let tarih = new Date();
-  let guncelYil = tarih.getFullYear();
-  document.getElementById("year").textContent = guncelYil;
-});
- 
-
-function gonderiolustur() {
-  alert(
-    "Gönderi Paylaşmak İçin Sosyal Medya Hesaplarımızdan birinden herhangi bir gönderimizin altına gönderi oluşturacağınızı belirtip başlık + içeriği yorum olarak yazın. En kısa sürede paylaşılır."
-  );
-}
-
-function bizeUlasin() {
-  alert("Bize şu an ulaşamazsınız çünkü biz de bilmiyoruz neredeyiz :)");
-
-
 console.log("SarıSite gizli bölgeye hoş geldin! Burası sadece meraklılara açık.");
-
-document.querySelector("#isimayarlabutonu").addEventListener("click", isimAyarla);
-document.querySelector("#gonderiolustur").addEventListener("click", gonderiolustur);
- }
 }
